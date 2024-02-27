f5af3dc85e7fe34dd4e7513fc064e46d08d718a6
Amaury Delille <amaurydelille92500@gmail.com>
2024-02-27 10:30:14 +0700

    migrated db

--- server/mongodb.js
+++ server/mongodb.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require('mongodb');
+/*const { MongoClient } = require('mongodb');
 
 const url = 'mongodb://localhost:27017/';
 const options = {
@@ -19,4 +19,31 @@ async function connectToDatabase() {
     }
 }
 
+module.exports = { connectToDatabase };*/
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = "mongodb+srv://root:e0fKr1TjvlO42z1Q@arthub.su4jp8a.mongodb.net/?retryWrites=true&w=majority&appName=arthub";
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function connectToDatabase() {
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB successfully');
+        return client.db("arthub");
+    } catch (error) {
+        console.error('Error when connecting to MongoDB:', error);
+        throw error;
+    }
+}
+
 module.exports = { connectToDatabase };
+
